043599ae9a8c0a3845c02658fec7aa27fdcd005e
iffydopsqueen <omaleweks@gmail.com>
2026-01-17 02:26:54 -0500

    added server configurations for jenkins and tomcat

---
+++ app/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Tomcat Deploy</title>
+</head>
+<body>
+  <h1>Tomcat is serving this app</h1>
+  <p>Deployment time: <%= new java.util.Date() %></p>
+</body>
+</html>
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,44 +1,44 @@
 # VPC
 resource "aws_vpc" "devops_vpc" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
-    tags = {
-      Name = "${var.project}-vpc"
-    }
+  tags = {
+    Name = "${var.project}-vpc"
+  }
 }
 
 # Subnet
 resource "aws_subnet" "devops_subnet" {
-    vpc_id            = aws_vpc.devops_vpc.id
-    cidr_block        = "10.0.1.0/24"
-    availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.devops_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-    tags = {
-      Name = "jenkins-public-subnet"
-    }
+  tags = {
+    Name = "jenkins-public-subnet"
+  }
 }
 
 # Internet gateway
 resource "aws_internet_gateway" "devops_igw" {
-    vpc_id = aws_vpc.devops_vpc.id
+  vpc_id = aws_vpc.devops_vpc.id
 
-    tags = {
-        Name = "jenkins-igw"
-    }
+  tags = {
+    Name = "jenkins-igw"
+  }
 }
 
 # Route table
 resource "aws_route_table" "devops_rt" {
-    vpc_id = aws_vpc.devops_vpc.id
+  vpc_id = aws_vpc.devops_vpc.id
 
-    route {
-        cidr_block      = "0.0.0.0/0"
-        gateway_id      = aws_internet_gateway.devops_igw.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.devops_igw.id
+  }
 
-    tags = {
-        Name = "jenkins-rt"
-    }
+  tags = {
+    Name = "jenkins-rt"
+  }
 }
 
 # Associate route table with subnet
@@ -126,22 +126,22 @@ resource "aws_security_group" "tomcat_sg" {
 
 # Inbound traffic is allowed on port 8080 (Tomcat) only from Jenkins SG
 resource "aws_vpc_security_group_ingress_rule" "tomcat_from_jenkins" {
-  security_group_id = aws_security_group.tomcat_sg.id
-  description       = "Tomcat from Jenkins SG"
+  security_group_id            = aws_security_group.tomcat_sg.id
+  description                  = "Tomcat from Jenkins SG"
   referenced_security_group_id = aws_security_group.jenkins_sg.id
-  from_port         = 8080
-  to_port           = 8080
-  ip_protocol       = "tcp"
+  from_port                    = 8080
+  to_port                      = 8080
+  ip_protocol                  = "tcp"
 }
 
 # Inbound traffic is allowed on port 22 (SSH) only from Jenkins SG - allow Jenkins to SSH into Tomcat for deployment
 resource "aws_vpc_security_group_ingress_rule" "tomcat_ssh_from_jenkins" {
-  security_group_id = aws_security_group.tomcat_sg.id
-  description       = "SSH from Jenkins SG for deployment"
+  security_group_id            = aws_security_group.tomcat_sg.id
+  description                  = "SSH from Jenkins SG for deployment"
   referenced_security_group_id = aws_security_group.jenkins_sg.id
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
+  from_port                    = 22
+  to_port                      = 22
+  ip_protocol                  = "tcp"
 }
 
 resource "aws_vpc_security_group_egress_rule" "tomcat_allow_all_traffic_ipv4" {
@@ -160,12 +160,12 @@ resource "aws_vpc_security_group_egress_rule" "tomcat_allow_all_traffic_ipv6" {
 
 # EC2 instance for Jenkins
 resource "aws_instance" "jenkins_instance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.devops_subnet.id
-  security_groups = [ aws_security_group.jenkins_sg.id ]
-  key_name        = var.key_pair_name
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.devops_subnet.id
+  security_groups             = [aws_security_group.jenkins_sg.id]
   associate_public_ip_address = true
+  key_name                    = var.key_pair_name
 
   tags = {
     Name = "${var.project}-jenkins"
@@ -175,11 +175,12 @@ resource "aws_instance" "jenkins_instance" {
 
 # EC2 instance for Tomcat
 resource "aws_instance" "tomcat_instance" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  subnet_id       = aws_subnet.devops_subnet.id
-  security_groups = [ aws_security_group.tomcat_sg.id ]
-  key_name        = var.key_pair_name
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.devops_subnet.id
+  security_groups             = [aws_security_group.tomcat_sg.id]
+  associate_public_ip_address = true
+  key_name                    = var.key_pair_name
 
   tags = {
     Name = "${var.project}-tomcat"
