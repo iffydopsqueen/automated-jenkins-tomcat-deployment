e2a17c0092d2faef7fb37aadf751dc72094afb59
iffydopsqueen <omaleweks@gmail.com>
2026-01-16 19:13:37 -0500

    added java app code

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,8 @@ output "tomcat_private_ip" {
   description = "The private IP address of the Tomcat server"
   value       = aws_instance.tomcat_instance.private_ip
 }
+
+output "tomcat_public_ip" {
+  description = "The public IP address of the Tomcat server"
+  value       = aws_instance.tomcat_instance.public_ip
+}
